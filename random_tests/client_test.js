255e5f2f980d5701ae59e2a6ef47cf87ea887fa2
andreluis034 <andreluis034@users.noreply.github.com>
2018-03-22 14:32:29 +0000

    rename test folder

--- random_tests/client_test.js
+++
@@ -1,4 +0,0 @@
-const PURDP = require('../')
-
-const prudp = new PRUDP("34.211.235.135", 60000, "ridfebb9", "TODO", 0, 0xAF, 0xA1);
-prudp.connect();
---
+++ tests/client_test.js
@@ -0,0 +1,4 @@
+const PURDP = require('../')
+
+const prudp = new PRUDP("127.0.0.1", 60000, "ridfebb9", "TODO", 0, 0xAF, 0xA1);
+prudp.connect();
